ae43c9ebba7b32f28fc654ea70b4b7eacf51b7a2
Martin Refseth <refshet@gmail.com>
2019-12-10 18:30:17 +0100

    Optimize code
    Optimized code to run even faster, cracking time should be down by about 5-6 days on high end systems (about 14-15 days now)

--- JoeyBruteforcer.py
+++ JoeyBruteforcer.py
@@ -1,28 +1,17 @@
 import string
-
 import usb.core
 import usb.util
 
 dev = usb.core.find(idVendor=0x046d, idProduct=0x1234)
 with open("JoeyList.txt") as f:
     for line in f:
-        #Uncomment the line below if you want to know what key is currently being tested, this will slow down the brute forcing process
-        #print(line)
-        Key=line[0:8]
-        B1=int(Key[0:2],16)
-        B2=int(Key[2:4],16)
-        B3=int(Key[4:6],16)
-        B4=int(Key[6:8],16)
-        B1=B1.to_bytes(1,'little')
-        B2=B2.to_bytes(1,'little')
-        B3=B3.to_bytes(1,'little')
-        B4=B4.to_bytes(1,'little')
-        WriteCommand=b'\x81'+B1+B2+B3+B4
-        dev.write(0x01,WriteCommand)
-        USBbuffer = dev.read(0x81,64)
+        B1=int(line[0:2],16).to_bytes(1,'little')
+        B2=int(line[2:4],16).to_bytes(1,'little')
+        B3=int(line[4:6],16).to_bytes(1,'little')
+        B4=int(line[6:8],16).to_bytes(1,'little')
+        dev.write(0x01,b'\x81'+B1+B2+B3+B4)
         dev.write(0x01,[0x84])
-        USBbuffer = dev.read(0x81,64)
-        if(USBbuffer[0] == 1):
+        if(dev.read(0x81,64)[0] == 1):
             print("Key Found, Outputting to key.txt")
             print(line)
             keyFile = open("key.txt", "w+")
