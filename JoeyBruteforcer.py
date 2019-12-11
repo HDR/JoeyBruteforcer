9c4cdf0d142ac24d647b31fdb8401b67f23ca68f
Martin Refseth <20230450+HDR@users.noreply.github.com>
2019-12-11 15:09:27 +0100

    Improved string manipulation code
    Thanks shash#1870

--- JoeyBruteforcer.py
+++ JoeyBruteforcer.py
@@ -5,11 +5,8 @@ import usb.util
 dev = usb.core.find(idVendor=0x046d, idProduct=0x1234)
 with open("JoeyList.txt") as f:
     for line in f:
-        B1=int(line[0:2],16).to_bytes(1,'little')
-        B2=int(line[2:4],16).to_bytes(1,'little')
-        B3=int(line[4:6],16).to_bytes(1,'little')
-        B4=int(line[6:8],16).to_bytes(1,'little')
-        dev.write(0x01,b'\x81'+B1+B2+B3+B4)
+        B=int(line[0:8],16).to_bytes(4,'big')
+        dev.write(0x01,b'\x81'+B)
         dev.write(0x01,[0x84])
         if(dev.read(0x81,64)[0] == 1):
             print("Key Found, Outputting to key.txt")
